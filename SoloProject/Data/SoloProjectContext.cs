8efb34bb5e1bf5805bc403dc992f42dd1c846917
Regis <ott.regisorate@gmail.com>
2023-07-17 06:27:29 +0800

    first commit

---
+++ SoloProject/Data/SoloProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SoloProject.Models;
+
+namespace SoloProject.Data
+{
+    public class SoloProjectContext : DbContext
+    {
+        public SoloProjectContext (DbContextOptions<SoloProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SoloProject.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ SoloProject/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SoloProject.Data;
+using SoloProject.Models;
+
+namespace SoloProject.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SoloProject.Data.SoloProjectContext _context;
+
+        public IndexModel(SoloProject.Data.SoloProjectContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
